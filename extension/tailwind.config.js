9572d81ef95257d1504d0ba7924703510afcdff1
Samy <kondasamy@outlook.com>
2025-01-29 21:33:16 +0530

    Get rid of boilerplate based codebase

--- extension/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  important: '#chrome-extension-root',
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
